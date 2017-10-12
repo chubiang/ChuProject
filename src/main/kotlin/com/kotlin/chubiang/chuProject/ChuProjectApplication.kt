ca350421d64eab6398a08121bb859b651d86be2c
DESKTOP-S4T0JA8\JIYEON <jiyeon@mine-k.co.kr>
2017-10-12 18:41:48 +0900

    merged chuProject

---
+++ src/main/kotlin/com/kotlin/chubiang/chuProject/ChuProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.kotlin.chubiang.chuProject
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ChuProjectApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(ChuProjectApplication::class.java, *args)
+}
